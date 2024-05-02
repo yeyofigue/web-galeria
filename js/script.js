3783e8e04af872af48af5617c5a5c93f329da050
Sergio Figueroa <yeyofigue1986@hotmail.com>
2024-05-02 14:45:29 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+
+    slidesPerView: 1,
+    spaceBetween: 80,
+    grabCursor: true,
+    loop: true,
+    breakpoints: {
+        991:{
+            slidesPerView: 3
+        }
+    }
+})
